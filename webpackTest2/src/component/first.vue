9ebb32033bf0f44793926385e4a9fad950c0aff7
admin <admin>
2017-03-16 18:54:01 +0800

    [webpack+vue]

---
+++ webpackTest2/src/component/first.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="first">
+        <h1>I am a title</h1>
+        <a>written by {{author}}</a>
+    </div>
+</template>
+<script type="text/javascript">
+    export default {
+        data() {
+            return {
+                author: "decre.liao zkteco"
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    
+</style>
