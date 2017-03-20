2b3db27aeddbb7971ad1b757a1945c38477e3578
admin <admin>
2017-03-20 14:31:34 +0800

    [third.vue]

---
+++ webpackTest2/src/component/third.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="third">
+        <h1>Third son page</h1>
+        <a>written by {{ author }}</a>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return{
+                author: "decre.liao zkteco"
+            }
+        }
+    }
+</script>
